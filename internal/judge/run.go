e1772083dec23f9a0d73f765377f7398c4c630d9
nglong14 <nguyenthanhlongltt@gmail.com>
2026-08-29 14:38:03 -0400

    feat(judge): wire judge workers into runner

--- internal/judge/run.go
+++ internal/judge/run.go
@@ -2,9 +2,14 @@ package judge
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nglong14/CodeDuel/internal/app"
+	"github.com/nglong14/CodeDuel/internal/redisx"
 )
 
 func Run(ctx context.Context, deps *app.Dependencies) error {
@@ -17,14 +22,109 @@ func Run(ctx context.Context, deps *app.Dependencies) error {
 			deps.Logger.Error("close Docker client", "err", err)
 		}
 	}()
+	queue := redisx.NewJudgeQueue(deps.Redis)
+	if err := queue.EnsureGroup(ctx); err != nil {
+		return fmt.Errorf("initialize judge queue: %w", err)
+	}
+	store, err := newPostgresStore(deps.Postgres)
+	if err != nil {
+		return err
+	}
+	consumerPrefix := judgeConsumerName()
+	workers := make([]*judgeService, deps.Config.Judge.Concurrency)
+	for index := range workers {
+		workers[index], err = newJudgeService(
+			deps.Logger,
+			queue,
+			store,
+			executor,
+			func(callCtx context.Context, channel string, payload []byte) error {
+				return deps.Redis.Publish(callCtx, channel, payload).Err()
+			},
+			fmt.Sprintf("%s-%d", consumerPrefix, index+1),
+			deps.Config.Judge.AttemptLease,
+			limitsFromConfig(deps.Config.Judge),
+		)
+		if err != nil {
+			return err
+		}
+	}
+	shutdownGrace := deps.Config.Judge.TotalTimeout + 2*deps.Config.Judge.CleanupTimeout
 
 	deps.Logger.Info("ready",
 		"sandbox_languages", 3,
-		"sandbox_concurrency", deps.Config.Judge.Concurrency,
+		"sandbox_concurrency", len(workers),
 		"sandbox_timeout", deps.Config.Judge.TotalTimeout,
+		"shutdown_grace", shutdownGrace,
+		"consumer_prefix", consumerPrefix,
 	)
-
-	<-ctx.Done()
+	err = runJudgeWorkers(ctx, workers, shutdownGrace)
 	deps.Logger.Info("shutting down")
+	return err
+}
+
+func runJudgeWorkers(ctx context.Context, workers []*judgeService, shutdownGrace time.Duration) error {
+	if len(workers) == 0 || shutdownGrace <= 0 {
+		return errors.New("run judge workers: invalid configuration")
+	}
+	readCtx, cancelReads := context.WithCancel(ctx)
+	defer cancelReads()
+	workCtx, cancelWork := context.WithCancel(context.WithoutCancel(ctx))
+	defer cancelWork()
+
+	done := make(chan error, len(workers))
+	for _, worker := range workers {
+		if worker == nil {
+			return errors.New("run judge workers: nil worker")
+		}
+	}
+	for _, worker := range workers {
+		go func() {
+			done <- worker.run(readCtx, workCtx)
+		}()
+	}
+
+	remaining := len(workers)
+	shutdownSignal := ctx.Done()
+	var (
+		firstErr  error
+		forceStop <-chan time.Time
+		timer     *time.Timer
+	)
+	for remaining > 0 {
+		select {
+		case err := <-done:
+			remaining--
+			if err != nil && !errors.Is(err, context.Canceled) && firstErr == nil {
+				firstErr = err
+				cancelReads()
+				timer = time.NewTimer(shutdownGrace)
+				forceStop = timer.C
+				shutdownSignal = nil
+			}
+		case <-shutdownSignal:
+			cancelReads()
+			timer = time.NewTimer(shutdownGrace)
+			forceStop = timer.C
+			shutdownSignal = nil
+		case <-forceStop:
+			cancelWork()
+			forceStop = nil
+		}
+	}
+	if timer != nil && !timer.Stop() && forceStop != nil {
+		<-timer.C
+	}
+	if firstErr != nil {
+		return firstErr
+	}
 	return ctx.Err()
+}
+
+func judgeConsumerName() string {
+	hostname, err := os.Hostname()
+	if err != nil || hostname == "" {
+		hostname = "unknown-host"
+	}
+	return fmt.Sprintf("%s-%d-%s", hostname, os.Getpid(), uuid.NewString())
 }
